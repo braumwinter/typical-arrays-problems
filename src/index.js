5e41e7da48df8038621b18e377bed517afc91152
Kate <braumwinter@gmail.com>
2020-03-17 10:31:47 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let min = array[0];
 
-exports.max = function max (array) {
-  return 0;
-}
+    for (let index in array) {
+        if (min > array[index]) {
+            min = array[index];
+        }
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return min;
+};
+
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let max = array[0];
+
+    array.forEach(function (item, index, array) {
+        if(max < item){
+          max = item;
+        }
+    });
+
+    return max;
+};
+
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let total = array.reduce(function(accumulator, currentValue) {
+      return accumulator + currentValue;
+    });
+
+    let avg = total / array.length;
+
+    return avg;
+};
